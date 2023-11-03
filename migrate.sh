aa7ebd69a0048ed8ab3114797a7c1fe54f2d82cc
Jake Runzer <jakerunzer@gmail.com>
2023-11-03 19:15:52 -0400

    delete metadata row to avoid conflicts

--- migrate.sh
+++ migrate.sh
@@ -111,6 +111,9 @@ echo "Dump file size: $dump_file_size"
 
 section "Restoring database to NEW_URL"
 
+# Delete the _timescaledb_catalog.metadata row that contains the exported_uuid to avoid conflicts
+psql $DATABASE_URL -c "DELETE FROM _timescaledb_catalog.metadata WHERE key = 'exported_uuid';"
+
 # Restore that data to the new database
 psql $NEW_URL -v ON_ERROR_STOP=1 --echo-errors \
     -f $dump_file || error_exit "Failed to restore database to $NEW_URL."
